a4263309504f76826dbdea53b759325d84b129bb
KungPaoChick <matthewriel63@gmail.com>
2021-09-09 20:09:51 +0800

    added all necessary configurations, packages, and quick installer for after minimal installation of Arch Linux.

---
+++ install.sh
@@ -0,0 +1,78 @@
+#!/bin/env bash
+set -e
+
+set_color() {
+    if [ -t 1 ]; then
+        RED=$(printf '\033[31m')
+        GREEN=$(printf '\033[32m')
+        YELLOW=$(printf '\033[33m')
+        BLUE=$(printf '\033[34m')
+        BOLD=$(printf '\033[1m')
+        RESET=$(printf '\033[m')
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        RESET=""
+    fi
+}
+
+main() {
+    set_color
+
+    reset
+    echo "${BLUE}"
+    echo "▄▄      ▄▄ ▄▄▄▄▄▄▄▄  ▄▄           ▄▄▄▄     ▄▄▄▄    ▄▄▄  ▄▄▄  ▄▄▄▄▄▄▄▄  ▄▄"; sleep 0.1
+    echo "██      ██ ██▀▀▀▀▀▀  ██         ██▀▀▀▀█   ██▀▀██   ███  ███  ██▀▀▀▀▀▀  ██"; sleep 0.1
+    echo "▀█▄ ██ ▄█▀ ██        ██        ██▀       ██    ██  ████████  ██        ██"; sleep 0.1
+    echo " ██ ██ ██  ███████   ██        ██        ██    ██  ██ ██ ██  ███████   ██"; sleep 0.1
+    echo " ███▀▀███  ██        ██        ██▄       ██    ██  ██ ▀▀ ██  ██        ▀▀"; sleep 0.1
+    echo " ███  ███  ██▄▄▄▄▄▄  ██▄▄▄▄▄▄   ██▄▄▄▄█   ██▄▄██   ██    ██  ██▄▄▄▄▄▄  ▄▄"; sleep 0.1
+    echo " ▀▀▀  ▀▀▀  ▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀     ▀▀▀▀     ▀▀▀▀    ▀▀    ▀▀  ▀▀▀▀▀▀▀▀  ▀▀"
+    echo "${RESET}"
+
+    # full upgrade
+    clear
+    printf "${GREEN}${BOLD}[*] ${RESET}Performing System Upgrade and Installation...\n\n"
+    sudo pacman -Syu --noconfirm
+
+    # install system packages
+    sudo pacman -S --needed --noconfirm - < pkgs.txt
+
+    # copy home dots
+    cp -f dots/.vimrc \
+          dots/.fehbg \
+          dots/.xinitrc $HOME
+
+    # create user directories
+    xdg-user-dirs-update
+
+    # copies all dwm configs to .config directory
+    if [[ ! -d $HOME/.config ]]; then
+        mkdir -p $HOME/.config
+        cp -rf configs/* $HOME/.config
+    else
+        cp -rf configs/* $HOME/.config
+    fi
+
+    # install dwm configs
+    if [[ -d $HOME/.config/dwm ]]; then
+        (cd $HOME/.config/dwm; sudo make clean install)
+    fi
+
+    # install st configs
+    if [[ -d $HOME/.config/st ]]; then
+        (cd $HOME/.config/st; sudo make clean install)
+    fi
+
+    # install dmenu configs
+    if [[ -d $HOME/.config/dmenu ]]; then
+        (cd $HOME/.config/dmenu; sudo make clean install)
+    fi
+
+    "${GREEN}${BOLD}[*] ${RESET}Everything has been set up for you, ${GREEN}$USER${RESET}"
+}
+
+main "@"
